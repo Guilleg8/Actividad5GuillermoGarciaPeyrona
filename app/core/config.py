398a66afcf8519b8bd71eba2673b9bc082bb5a5c
Guilleg8 <ggarcpey@myuax.com>
2025-11-24 19:55:32 +0100

    Initial commit

---
+++ app/core/config.py
@@ -0,0 +1,8 @@
+APP_NAME = "Jurassic Park Monitor v1.0"
+
+# Umbrales de Alerta
+ALERT_THRESHOLDS = {
+    "temperatura": 42.0,      # Grados Celsius
+    "cardiaco": 180,          # BPM
+    "movimiento": 90          # Velocidad relativa
+}
---
+++ app/core/logger.py
@@ -0,0 +1,4 @@
+from aiologger import Logger
+
+# Instancia global del logger
+logger = Logger.with_default_handlers(name="jurassic_logger")
